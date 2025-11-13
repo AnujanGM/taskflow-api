b3c6e1065e136f530311682c092e2d005e8e3417
Anujan <anujan1352@gmail.com>
2025-11-13 22:58:56 +0530

    feat: add Task entity

---
+++ src/main/java/com/taskflow/api/model/Task.java
@@ -0,0 +1,26 @@
+package com.taskflow.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+}
